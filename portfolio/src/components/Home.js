cf4ac050f8b9782da4e2d53d7604459bea9fa16f
carlsachs <sachscarl@gmail.com>
2020-07-10 01:10:44 -0400

    progress using bootstrap and setting up projects page

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 //import styling files
+import Nav from "./Nav";
+
 import "./Home.css";
 
 const Home = () => {
 
     return (
         <div className="topdiv">
+            <Nav />
             <div className="title">
                 <h1 carl-label="../Carl/Sachs"></h1>
             </div>
---
+++ portfolio/src/components/Nav.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+//import bootstrap
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { NavDropdown } from "react-bootstrap";
+
+const Nav1 = () => {
+    return (
+        <Navbar fixed="top" collapseOnSelect expand="lg" bg="light" variant="light">
+            <Navbar.Brand href="/">Carl's Portfolio</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                <Nav.Link href="#features">About Me</Nav.Link>
+                <Nav.Link href="/projects">Projects</Nav.Link>
+                <NavDropdown title="Contact Me" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="https://mail.google.com/mail/?view=cm&fs=1&to=sachscarl@gmail.com&su=Inquiry&body=Thank you for taking the time to send me a message.  I will get back to you promptly!" target="_blank">Send me an Email</NavDropdown.Item>
+                    <NavDropdown.Item href="https://www.twitter.com/CarltonSachs" target="_blank">Follow me on Twitter</NavDropdown.Item>
+                    <NavDropdown.Item href="https://www.facebook.com/CarlSachsWeb/" target="_blank">Message me on FaceBook</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="https://www.linkedin.com/in/carl-sachs/" target="_blank">Connect on LinkedIn</NavDropdown.Item>
+                </NavDropdown>
+                </Nav>
+                <Nav>
+                <Nav.Link href="https://www.linkedin.com/in/carl-sachs/" target="_blank">LinkedIn</Nav.Link>
+                <Nav.Link href="https://github.com/carlsachs" target="_blank">GitHub</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                    Resume
+                </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default Nav1;
+
+

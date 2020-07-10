7d5116fc2bd3199783ee9851c9b37abf0a167aa1
carlsachs <sachscarl@gmail.com>
2020-07-10 02:13:07 -0400

    med and insta cards done

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -7,6 +7,8 @@ import { Fade, Flip } from "react-reveal";
 //other components
 import Readrr from "./Procards/Readrr";
 import Bootwork from "./Procards/Bootwork";
+import MedCabinet from "./Procards/MedCabinet";
+import InstaClone from "./Procards/InstaClone";
 
 //images
 import undraw from "./undraw.svg";
@@ -69,74 +71,15 @@ const Projects = () => {
             <Col>
                 <Bootwork />
             </Col>
+            <Col>
+                <MedCabinet />
+            </Col>
+            <Col>
+                <InstaClone />
+            </Col>
         </Row>
         </Container>
     )
 }
 
 export default Projects;
-
-
-// <div className="front-end">
-// <div className="front-top">
-//     <h2 style={{marginTop: "2%", textDecoration: "none", color: "#003a8c", border: "1px solid #003a8c", padding: "10px", borderRadius: "10px", width: "20%", margin: "0 auto", textAlign: "center"}}>Front-End</h2>
-// </div>
-// <div className="front-bottom">
-//     <Slide left>
-//         <FE2 />
-//     </Slide>
-//     <Slide right>
-//         <FE3 />
-//     </Slide>
-//     <Slide left>
-//         <FE1 />
-//     </Slide>
-//     <Slide right>
-//         <FE4 />
-//     </Slide>
-//     <Slide left>
-//         <FE5 />
-//     </Slide>
-// </div>
-// </div>
-// <div className="back-end">
-// <div className="back-top">
-//     <h2 style={{marginTop: "2%", textDecoration: "none", color: "#003a8c", border: "1px solid #003a8c", padding: "10px", borderRadius: "10px", width: "20%", margin: "0 auto"}}>Back-End</h2>
-// </div>
-// <div className="front-bottom">
-//     <Slide left>
-//         <BE1 />
-//     </Slide>
-//     <Slide right>
-//         <BE2 />
-//     </Slide>
-//     <Slide left>
-//         <BE3 />
-//     </Slide>
-//     <Slide right>
-//         <BE4 />
-//     </Slide>
-// </div>
-// </div>
-// <div className="CS">
-// <div className="CS-top">
-//     <h2 style={{marginTop: "2%", textDecoration: "none", color: "#003a8c", border: "1px solid #003a8c", padding: "10px", borderRadius: "10px", width: "20%", margin: "0 auto"}}>Computer Science</h2>
-// </div>
-// <div className="CS-bottom">
-//     <Slide left>
-//         <FE2 />
-//     </Slide>
-//     <Slide right>
-//         <FE3 />
-//     </Slide>
-//     <Slide left>
-//         <FE1 />
-//     </Slide>
-//     <Slide right>
-//         <FE4 />
-//     </Slide>
-//     <Slide left>
-//         <FE5 />
-//     </Slide>
-// </div>
-// </div>

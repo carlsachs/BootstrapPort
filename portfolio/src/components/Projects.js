ed533492b04c1404d762bf2b85c5661b5b4aebaf
carlsachs <sachscarl@gmail.com>
2020-07-10 03:46:49 -0400

    footer and social in footer done for now off for night

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 
 //import resources
 import { Link } from "react-router-dom";
-import { Fade, Flip } from "react-reveal";
+import { Fade, Flip, Slide } from "react-reveal";
 
 //other components
 import Readrr from "./Procards/Readrr";
@@ -12,6 +12,8 @@ import InstaClone from "./Procards/InstaClone";
 import Middleware from "./Procards/Middleware";
 import Todo from "./Procards/Todo";
 import Db3 from "./Procards/Db3";
+import Authentication from "./Procards/Authentication";
+import Footer from "./Footer";
 
 //images
 import undraw from "./undraw.svg";
@@ -68,40 +70,77 @@ const Projects = () => {
             </div>
         </Row>
         <Row className="front-boxes">
+            <Fade bottom>
             <Col>
                 <Readrr />
             </Col>
+            </Fade>
+            <Fade bottom>
             <Col>
                 <Bootwork />
             </Col>
+            </Fade>
+            <Fade bottom>
             <Col>
                 <MedCabinet />
             </Col>
+            </Fade>
+            <Fade bottom>
             <Col>
                 <InstaClone />
             </Col>
+            </Fade>
         </Row>
-        <Row className="this">
-            <div className="port">
-                <h2>To view the source code of this very portfolio, click <Link target="_blank" to="https://github.com/carlsachs/BootstrapPort" style={{color: "#69c0ff"}}>here</Link>.</h2>
-            </div>   
-        </Row>
+        <Slide left>
+            <Row className="this">
+                <div className="port">
+                    <h2>To view the source code of this very portfolio, click <Link target="_blank" to="https://github.com/carlsachs/BootstrapPort" style={{color: "#69c0ff"}}>here</Link>.</h2>
+                </div>   
+            </Row>
+        </Slide>
         <Row className="back-top">
             <div>
                 <h2 style={{marginTop: "2%", textDecoration: "none", color: "#003a8c", border: "1px solid #003a8c", padding: "10px", borderRadius: "10px", textAlign: "center"}}>Back End</h2>
             </div>
         </Row>
-        <Row className="back-boxes">
-            <Col>
-                <Middleware />
-            </Col>
-            <Col>
-                <Todo />
-            </Col>
-            <Col>
-                <Db3 />
-            </Col>
+        <Slide right>
+            <Row className="back-boxes">
+                <Col>
+                    <Middleware />
+                </Col>
+                <Col>
+                    <Todo />
+                </Col>
+                <Col>
+                    <Db3 />
+                </Col>
+                <Col>
+                    <Authentication />
+                </Col>
+            </Row>
+        </Slide>
+        <Row className="cs-top">
+            <div>
+                <h2 style={{marginTop: "2%", textDecoration: "none", color: "#003a8c", border: "1px solid #003a8c", padding: "10px", borderRadius: "10px", textAlign: "center"}}>Computer Science</h2>
+            </div>
         </Row>
+        <Slide left>
+            <Row className="cs-boxes">
+                <Col>
+                    <Readrr />
+                </Col>
+                <Col>
+                    <Bootwork />
+                </Col>
+                <Col>
+                    <MedCabinet />
+                </Col>
+                <Col>
+                    <InstaClone />
+                </Col>
+            </Row>
+        </Slide>
+        <Footer />
         </Container>
     )
 }

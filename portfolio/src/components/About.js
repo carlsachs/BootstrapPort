a008750ae64b283e9af85dc0ea791af923055954
carlsachs <sachscarl@gmail.com>
2020-07-12 22:18:13 -0400

    small changes in various files

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -4,7 +4,8 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Carousel from 'react-bootstrap/Carousel'
+import ProgressBar from "react-bootstrap/ProgressBar";
+
 
 import "./About.css";
 
@@ -15,6 +16,7 @@ import minnesota from "../images/minnesota.jpg";
 
 //import other components
 import Nav1 from "./Nav";
+import Footer from "./Footer";
 
 const About = () => {
 
@@ -34,6 +36,21 @@ const About = () => {
                    <h2 style={{textAlign: "center", color: "aliceblue", fontSize: "3rem" }}>Image of myself here</h2>
                 </Col>
             </Row>
+            <Row className="track">
+                <Col className="left-track">
+                    <h2>Life at Lambda</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </Col>
+                <Col className="right-track">
+                <Row>
+                    <h2>Completion</h2>
+                </Row>
+                <Row>
+                <p>Track my completion of the Lambda Full-Stack Development course.</p>
+                </Row>
+                </Col>
+            </Row>
+            <Footer />
         </Container>
     )
 }

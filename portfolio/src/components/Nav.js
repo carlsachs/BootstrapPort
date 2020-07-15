655d0a1bb1bf6627cb00cb581dcf92fdd2a5ff3f
carlsachs <sachscarl@gmail.com>
2020-07-15 19:14:15 -0400

    navbar responsiveness fixes

--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -24,7 +24,6 @@ const Nav1 = () => {
                 <NavDropdown title="Contact Me" id="collasible-nav-dropdown">
                     <NavDropdown.Item href="https://mail.google.com/mail/?view=cm&fs=1&to=sachscarl@gmail.com&su=Inquiry&body=Thank you for taking the time to send me a message.  I will get back to you promptly!" target="_blank">Send me an Email</NavDropdown.Item>
                     <NavDropdown.Item href="https://www.twitter.com/CarltonSachs" target="_blank">Follow me on Twitter</NavDropdown.Item>
-                    <NavDropdown.Item href="https://www.facebook.com/CarlSachsWeb/" target="_blank">Message me on FaceBook</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="https://www.linkedin.com/in/carl-sachs/" target="_blank">Connect on LinkedIn</NavDropdown.Item>
                 </NavDropdown>

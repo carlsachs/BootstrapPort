b40834dfc74d0536cf57ee2b4f7cb41fcf459943
carlsachs <sachscarl@gmail.com>
2020-07-17 10:39:12 -0400

    added top of skills page to site

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-//import bootstrap and css
+//import bootstrap and css / other styling components
 import "./Skills.css";
 import Container from "react-bootstrap/Container";
 import { Row, Col } from "react-bootstrap";
+import { Fade } from "react-reveal";
 
 //import other components
 import Nav from "./Nav.js";
@@ -14,11 +15,21 @@ const Skills = () => {
         <Container fluid>
         <Nav />
             <Row className="top-skills">
-                <h2>Skills</h2>
+                
+                <Row className="skills-title">
+                    <Fade top>
+                        <h2>Skills</h2>
+                    </Fade>
+                </Row>
+                <Row className="skills-desc">
+                    <Fade bottom>
+                        <h3>Lambda is fast-paced, and full of resource.  Done right, it can set you up to learn any concept in programming and adapt to the environment quickly and efficiently.</h3>
+                    </Fade>
+                    </Row>
             </Row>
             <Row className="skillbox">
                 <Col>
-                    
+
                 </Col>
             </Row>
         </Container>

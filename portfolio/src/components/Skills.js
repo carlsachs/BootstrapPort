7c13d6404487c231efd86f5e09d591c1236e38a7
carlsachs <sachscarl@gmail.com>
2020-07-16 23:52:51 -0400

    navbar color change (again)

---
+++ portfolio/src/components/Skills.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+//import bootstrap and css
+import "./Skills.css";
+
+//import other components
+import Nav from "./Nav.js";
+
+const Skills = () => {
+
+    return (
+        <Container fluid>
+            <Row className="top-skills">
+                
+            </Row>
+        </Container>
+    )
+}
+
+export default Skills;
